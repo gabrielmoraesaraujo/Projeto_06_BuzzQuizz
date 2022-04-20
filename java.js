fe7c879b5bf0a742d7583747cb81e92dded3554c
gabriel <gmoraes.mat@gmail.com>
2022-04-20 11:36:52 -0300

    ajustando versões

--- java.js
+++ java.js
@@ -1,34 +1,21 @@
-
-
-
-
-
-
-
-
-
-
-
 function novosQuizzes(){
     const caixa = document.querySelector('.novosQuizzes');
     caixa.innerHTML = `
         <li class="quizz">
             <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
             <div>Futurama Muito brabo</div>
-        </li>    
+        </li>   
+        <li class="quizz">
+            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
+            <div>Futurama Muito brabo</div>
+        </li> 
+        <li class="quizz">
+            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
+            <div>Futurama Muito brabo</div>
+        </li>   
+         
     `
 }
 
 
 novosQuizzes();
-
-
-
-
-
-
-
-
-
-
-
