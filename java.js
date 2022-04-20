2ba1459981e8200f4d3534c00cd60f4cf1f7cdc0
Bismarck <bismarckdados@gmail.com>
2022-04-20 15:39:44 -0300

    Pegando as images e Titulos da API e colocando no Layout

--- java.js
+++ java.js
@@ -1,41 +1,36 @@
-
-function novosQuizzes(){
+let quizzes;
+const API = 'https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes';
+function mostrarQuizz(resposta){
     const caixa = document.querySelector('.novosQuizzes');
-    caixa.innerHTML = `
+    for(let i = 0; i < resposta.data.length; i++){
+        caixa.innerHTML += `
         <li class="quizz">
-            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
-            <div>Futurama Muito brabo</div>
+            <img src="${resposta.data[i].image}">
+            <div>${resposta.data[i].title}</div>
         </li>   
         <li class="quizz">
-            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
-            <div>Futurama Muito brabo</div>
+            <img src="${resposta.data[i].image}">
+            <div>${resposta.data[i].title}</div>
         </li> 
         <li class="quizz">
-            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
-            <div>Futurama Muito brabo</div>
+            <img src="${resposta.data[i].image}">
+            <div>${resposta.data[i].title}</div>
         </li> 
-        <li class="quizz">
-            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
-            <div>Futurama Muito brabo</div>
-        </li>   
-        <li class="quizz">
-            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
-            <div>Futurama Muito brabo</div>
-        </li> 
-        <li class="quizz">
-            <img src="https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-futura-nova-temporada.jpg">
-            <div>Futurama Muito brabo</div>
-        </li>     
-         
-    `
+        `
+    }
+    
 }
 
-
-novosQuizzes();
-
-
-
-
+// pega os quizz salvos na API
+function pegarQuizz(){
+    quizzes = axios.get(API);
+    quizzes.then(mostrarQuizz);
+    quizzes.catch(mostrarErro);
+}
+pegarQuizz();
+function mostrarErro(){
+    alert('Deu algum erro :(');
+}
 
 
 
