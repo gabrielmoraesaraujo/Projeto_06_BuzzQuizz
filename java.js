09657dc6a2ca917568f2e2806c5578a307b458a7
Bismarck <bismarckdados@gmail.com>
2022-04-21 19:13:55 -0300

    Adicionando troca de tela entre a Tela 1 e a Tela 2

--- java.js
+++ java.js
@@ -1,5 +1,12 @@
 let quizzes;
-const API = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes';
+const API = 'https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes';
+
+
+function iniciarPagina(){
+    
+}
+
+
 function mostrarQuizz(resposta){
     const caixa = document.querySelector('.novosQuizzes');
     for(let i = 0; i < resposta.data.length; i++){
@@ -27,7 +34,16 @@ function mostrarErro(){
     alert('Deu algum erro :(');
 }
 
+function chamarTela2(){
+    const tela2 = document.querySelector('.informacoes-basicas');
+    removerTela1()
+    tela2.classList.remove('escondido');
+}
 
+function removerTela1(){
+    const tela1 = document.querySelector('.tela1');
+    tela1.classList.add('escondido');
+}
 
 
 
